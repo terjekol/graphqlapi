2d38f80fca6efca2fdcf3246dfe77241463a85a2
Terje Kolderup <terje@kolderup.net>
2019-09-23 19:35:59 +0200

    Update FetchData.js

--- Api/ClientApp/src/components/FetchData.js
+++ Api/ClientApp/src/components/FetchData.js
@@ -10,58 +10,19 @@ const EXCHANGE_RATES = gql`
   }
   `;
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+export function FetchData() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
 
-  constructor(props) {
-    super(props);
-    const query = useQuery(EXCHANGE_RATES);
-    this.state = { forecasts: [], loading: true, query: query };
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+    return <p>data</p>
 
-    // fetch('api/SampleData/WeatherForecasts')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({ forecasts: data, loading: false });
-    //   });
-  }
-
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table table-striped'>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
-
-  render() {
-    let query = this.state.qyery;
-    let contents = query.loading ? <p><em>Loading...</em></p> :
-      query.error ? <p><em>Error...{query.error}</em></p> :
-        <p>data!</p>;
-
-    return (
-      <div>
-        <h1>Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
+  // return data.rates.map(({ currency, rate }) => (
+  //   <div key={currency}>
+  //     <p>
+  //       {currency}: {rate}
+  //     </p>
+  //   </div>
+  // ));
 }
