7a4b46f9a375fd910d4a77df7004f2836d13a9de
Terje Kolderup <terje@kolderup.net>
2019-09-23 07:55:10 +0200

    x

--- Api/ClientApp/src/index.js
+++ Api/ClientApp/src/index.js
@@ -6,30 +6,33 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import ApolloClient from 'apollo-boost';
 import { gql } from "apollo-boost";
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
   uri: '/graphql',
 });
 
 client.query({
-    query: gql`
+  query: gql`
     {
       author(id: 1) {
         name
       }
     }
     `
-  })
+})
   .then(result => console.log(result))
-  .catch(error=>console.error(error));
+  .catch(error => console.error(error));
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   rootElement);
 
 registerServiceWorker();
